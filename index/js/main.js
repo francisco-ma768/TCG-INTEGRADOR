1e360eabaf6b9c5d150c2010afee5f739eaba4ab
francisco-ma768 <fansiomarin@gmail.com>
2025-10-30 11:46:20 -0300

    elminar  stroy

---
+++ index/js/main.js
@@ -0,0 +1,13 @@
+function irATablero() {
+  window.location.href = "tablero.html";
+}
+
+function verCreditos() {
+  alert("Creadores: Martín, Mauro, Oliva y Armijo");
+}
+
+function verConfiguracion() {
+  alert("Configuraciones del juego próximamente...");
+}
+
+
