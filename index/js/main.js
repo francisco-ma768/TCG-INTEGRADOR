aff32cb701ea963d6e13f93bac8fd7d02dbe6a87
francisco-ma768 <fansiomarin@gmail.com>
2025-11-04 03:07:01 -0300

    sprint 4

--- index/js/main.js
+++ index/js/main.js
@@ -1,13 +1,1 @@
-function irATablero() {
-  window.location.href = "tablero.html";
-}
-
-function verCreditos() {
-  alert("Creadores: Martín, Mauro, Oliva y Armijo");
-}
-
-function verConfiguracion() {
-  alert("Configuraciones del juego próximamente...");
-}
-
 
